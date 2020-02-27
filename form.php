6fb009f6a34a5978533493ec3dab7d74fe8b4390
daan <2087884@talnet.nl>
2020-02-27 14:55:37 +0100

    Uitwerking van de exercise

---
+++ form.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="form.php" method="post" name="lijst">
+    <input type="text" name="email">
+    <input type="submit" name="submit">
+</form>
+    
+<?php
+
+
+$email = $_POST['email'];
+
+
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo("$email is een geldig email adres");
+  } else {
+    echo("$email is niet een geldig email adres");
+  }
+?>
+</body>
+</html>
+
+
